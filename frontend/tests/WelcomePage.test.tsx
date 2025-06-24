03e5a97065a99684974230981b626dae0747419b
aduong3 <mythalink@gmail.com>
2025-06-23 18:26:22 -0700

    (test): Add more test cases to WelcomePage.test.tsx

--- frontend/tests/WelcomePage.test.tsx
+++ frontend/tests/WelcomePage.test.tsx
@@ -3,6 +3,9 @@ import WelcomePage from "../pages/WelcomePage";
 
 const consoleLogSpy = jest.spyOn(console, "log").mockImplementation();
 
+// chatroom empty, nickname empty, both empty, special chars in nickname, special chars in chatroom id
+// both valid
+
 describe("WelcomePage", () => {
   test("renders without crashing", () => {
     render(<WelcomePage />);
@@ -44,12 +47,42 @@ describe("WelcomePage", () => {
       fireEvent.change(screen.getByPlaceholderText(/nickname/i), {
         target: { value: "beaverthelever" },
       });
-      fireEvent.change(screen.getByPlaceholderText(/chatroom/i), {
+      fireEvent.change(screen.getByPlaceholderText(/chatroom id/i), {
         target: { value: "room1" },
       });
     });
     fireEvent.click(screen.getByText(/submit/i));
     expect(consoleLogSpy).toHaveBeenCalledWith("beaverthelever", "room1");
+  });
+
+  test("does not submit if there are special characters in nickname", () => {
+    render(<WelcomePage />);
+    act(() => {
+      fireEvent.change(screen.getByPlaceholderText(/nickname/i), {
+        target: { value: "beaver@lever" },
+      });
+      fireEvent.change(screen.getByPlaceholderText(/chatroom id/i), {
+        target: { value: "room1" },
+      });
+    });
+    expect(consoleLogSpy).toHaveBeenCalledWith(
+      "Information is empty or not alphanumeric"
+    );
+  });
+
+  test("does not submit if there are special characters in chatroom", () => {
+    render(<WelcomePage />);
+    act(() => {
+      fireEvent.change(screen.getByPlaceholderText(/nickname/i), {
+        target: { value: "beaverthelever" },
+      });
+      fireEvent.change(screen.getByPlaceholderText(/chatroom id/i), {
+        target: { value: "room1!" },
+      });
+    });
+    expect(consoleLogSpy).toHaveBeenCalledWith(
+      "Information is empty or not alphanumeric"
+    );
   });
 
   afterAll(() => {
